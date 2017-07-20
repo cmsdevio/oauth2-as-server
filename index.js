8f523426d235d7a26d511aa9aa5ba7c1f11a2ff1
philcms1 <phil@soaarchitects.com>
2017-07-20 14:25:08 -0600

    Basic handlebars views working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Confidence = require('confidence');
 const Glue = require('glue');
 const config = require('./config');
 
+
 const store = new Confidence.Store(config);
 const Manifest = store.get('/', { env: process.env.NODE_ENV });
 
@@ -12,11 +13,14 @@ Glue.compose(Manifest, options, (err, server) => {
   if (err) {
     throw err;
   }
+
+
+
     // server.app.db = dbConn;
   server.start((error) => {
     if (error) {
       throw error;
     }
-    console.log(`Server started at: ${server.info.uri}`);
+    console.log(`Server started at: ${server.info}`);
   });
 });
