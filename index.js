686c26d7b6788e3e7aa5f7236bec0656426f463e
philcms1 <phil@soaarchitects.com>
2017-07-21 07:50:53 -0600

    Small cleanup

--- index.js
+++ index.js
@@ -2,25 +2,19 @@ const Confidence = require('confidence');
 const Glue = require('glue');
 const config = require('./config');
 
-
 const store = new Confidence.Store(config);
 const Manifest = store.get('/', { env: process.env.NODE_ENV });
-
 const options = { relativeTo: __dirname };
-// const db = require('./lib/core/database');
 
 Glue.compose(Manifest, options, (err, server) => {
-  if (err) {
-    throw err;
-  }
-
-
-
-    // server.app.db = dbConn;
-  server.start((error) => {
-    if (error) {
-      throw error;
+    if (err) {
+        throw err;
     }
-    console.log(`Server started at: ${server.info}`);
-  });
+
+    server.start((error) => {
+        if (error) {
+            throw error;
+        }
+        console.log(`Server started...`);
+    });
 });
