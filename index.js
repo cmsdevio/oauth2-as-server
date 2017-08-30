3c76acb8209c088223de949e2970fd02bc8d6c4f
philcms1 <phil@soaarchitects.com>
2017-08-29 18:47:07 -0600

    Fixing configuration

--- index.js
+++ index.js
@@ -3,7 +3,67 @@ const Glue = require('glue');
 const config = require('./config');
 
 const store = new Confidence.Store(config);
-const Manifest = store.get('/', { env: process.env.NODE_ENV });
+
+// const Manifest = store.get('/', { env: process.env.NODE_ENV });
+const Manifest = {
+    server: {},
+    connections: [{
+        port: process.env.PORT || 9005,
+        labels: ["oauth2"],
+        routes: { cors: true }
+    }],
+    registrations: [
+        {
+            plugin: {
+                register: "good",
+                options: {
+                    reporters: {
+                        console: [
+                            {
+                                module: "good-squeeze",
+                                name: "Squeeze",
+                                args: [{
+                                    log: "*",
+                                    request: "*",
+                                    response: ["oauth2-*", "ui-*"]
+                                }]
+                            },
+                            {
+                                module: "good-console"
+                            },
+                            "stdout"
+                        ]
+                    }
+                }
+            }
+        },
+        {
+            plugin: "vision"
+        },
+        {
+            plugin: "inert"
+        },
+        {
+            plugin: "lout"
+        },
+        {
+            plugin: "hapi-auth-cookie"
+        },
+        {
+            plugin: "hapi-auth-bearer-token"
+        },
+        {
+            plugin: "./lib/modules/authServer/index",
+            options: {
+                select: ["oauth2"],
+                routes: {
+                    prefix: "/oauth2"
+                }
+            }
+        }
+    ]
+};
+
 const options = { relativeTo: __dirname };
 
 Glue.compose(Manifest, options, (err, server) => {
