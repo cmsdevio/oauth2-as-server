b68d9bc3714e0fa11672750eccf1e5c013d585fc
philcms1 <phil@soaarchitects.com>
2017-12-28 10:30:27 -0700

    Completed DCR unit tests

--- test/dcr.js
+++ test/dcr.js
@@ -98,7 +98,18 @@ const dcrReqData = {
     response_type: 'token',
     token_endpoint_auth_method: 'client_secret_basic'
 };
-
+const expectedPayload = {
+    client_name: 'restlet_client_5327',
+    redirect_uris: [ 'http://localhost:1234/dummy' ],
+    grant_types: [ 'client_credentials' ],
+    response_type: 'token',
+    token_endpoint_auth_method: 'client_secret_basic',
+    client_id: '9WBlEWnkLSwkcsTXI97bHfhx5joxleogWyK',
+    client_secret: 't6nadULs7u3hLDDmC2JzCuQJEFCxxRMthXMIF57OJhGTXm9nM4',
+    client_id_created_at: 1513791723,
+    client_secret_expires_at: 0,
+    isActive: true
+};
 // *************************************************
 // TESTING SUITE
 // *************************************************
@@ -126,18 +137,6 @@ suite('Dynamic Client Registration', () => {
     });
 
     test('should dynamically register a client', () => {
-        const expectedPayload = {
-            client_name: 'restlet_client_5327',
-            redirect_uris: [ 'http://localhost:1234/dummy' ],
-            grant_types: [ 'client_credentials' ],
-            response_type: 'token',
-            token_endpoint_auth_method: 'client_secret_basic',
-            client_id: '9WBlEWnkLSwkcsTXI97bHfhx5joxleogWyK',
-            client_secret: 't6nadULs7u3hLDDmC2JzCuQJEFCxxRMthXMIF57OJhGTXm9nM4',
-            client_id_created_at: 1513791723,
-            client_secret_expires_at: 0,
-            isActive: true
-        };
         const request = { method: 'POST', url: '/oauth2/register', payload: dcrReqData };
 
         server.inject(request, (res) => {
@@ -154,24 +153,10 @@ suite('Dynamic Client Registration', () => {
         });
     });
 
-    test('should populate the DCR client with default value for token endpoint', () => {
-        const expectedPayload = {
-            client_name: 'restlet_client_5327',
-            redirect_uris: [ 'http://localhost:1234/dummy' ],
-            grant_types: [ 'client_credentials' ],
-            response_type: 'token',
-            token_endpoint_auth_method: 'client_secret_basic',
-            client_id: '9WBlEWnkLSwkcsTXI97bHfhx5joxleogWyK',
-            client_secret: 't6nadULs7u3hLDDmC2JzCuQJEFCxxRMthXMIF57OJhGTXm9nM4',
-            client_id_created_at: 1513791723,
-            client_secret_expires_at: 0,
-            isActive: true
-        };
+    test('should populate the DCR client with default values', () => {
         const dcrReqDataNoTokenEndpointAuth = {
             client_name: 'restlet_client_5328',
-            redirect_uris: [ 'http://localhost:1234/dummy' ],
-            grant_types: [ 'client_credentials' ],
-            response_type: 'token'
+            redirect_uris: [ 'http://localhost:1234/dummy' ]
         };
         const request = { method: 'POST', url: '/oauth2/register', payload: dcrReqDataNoTokenEndpointAuth };
 
