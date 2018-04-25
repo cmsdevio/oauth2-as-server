2452dbda3a08b03d2ce5bc60668fdec03c23852d
philcms1 <phil@cmsdev.io>
2018-04-25 16:38:55 -0600

    Misc

--- lib/modules/authServer/src/main.jsx
+++ lib/modules/authServer/src/main.jsx
@@ -9,6 +9,7 @@ import ReduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { logger } from 'redux-logger';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
 
 import reducers from './reducers';
 import Header from './components/Header.component';
@@ -17,6 +18,7 @@ import ClientsList from './components/Clients/ClientsList.component';
 import AddClient from './components/Clients/AddClient.component';
 import UsersList from './components/Users/UsersList.component';
 import AddUser from './components/Users/AddUser.component';
+import { AS_AUTH_RESET } from './actions/types';
 
 const history = createHistory();
 const middleware = [ Promise, ReduxThunk, logger ];
@@ -25,8 +27,32 @@ const store = createStore(
     undefined,
     compose(applyMiddleware(...middleware, routerMiddleware(history)))
 );
+
+// TODO: extract this to its own file -- i.e.: network-utils
 // Export store for Axios configuration (token access)
-export default store;
+// export default store;
+const select = state => state.auth.token;
+
+const handleStoreChanges = () => {
+    const token = select(store.getState());
+    axios.defaults.headers.common.Authorization = `Bearer ${ token }`;
+};
+store.subscribe(handleStoreChanges);
+axios.interceptors.response.use((response) => {
+    // Do something with response data
+    return response;
+}, (error) => {
+    // Do something with response error
+    if (error.response.data.message === 'Bad token' &&
+        error.response.data.statusCode === 401) {
+        store.dispatch({ type: AS_AUTH_RESET });
+    }
+    return error;
+});
+
+module.exports.api = axios.create({
+    baseURL: process.env.ADMIN_BASE_URL || 'https://localhost:8443/admin'
+});
 
 const App = () => (
     <Provider store={store}>
