48d9d5ca06abbe0be938c80c285236c6b063b238
philcms1 <phil@cmsdev.io>
2018-04-25 17:43:49 -0600

    Temp state

--- lib/modules/authServer/src/main.jsx
+++ lib/modules/authServer/src/main.jsx
@@ -30,29 +30,7 @@ const store = createStore(
 
 // TODO: extract this to its own file -- i.e.: network-utils
 // Export store for Axios configuration (token access)
-// export default store;
-const select = state => state.auth.token;
-
-const handleStoreChanges = () => {
-    const token = select(store.getState());
-    axios.defaults.headers.common.Authorization = `Bearer ${ token }`;
-};
-store.subscribe(handleStoreChanges);
-axios.interceptors.response.use((response) => {
-    // Do something with response data
-    return response;
-}, (error) => {
-    // Do something with response error
-    if (error.response.data.message === 'Bad token' &&
-        error.response.data.statusCode === 401) {
-        store.dispatch({ type: AS_AUTH_RESET });
-    }
-    return error;
-});
-
-module.exports.api = axios.create({
-    baseURL: process.env.ADMIN_BASE_URL || 'https://localhost:8443/admin'
-});
+export default store;
 
 const App = () => (
     <Provider store={store}>
