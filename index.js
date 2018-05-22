db4564ab5a1e54591e2d935a71878d72d8d6fd7d
philcms1 <phil@cmsdev.io>
2018-05-22 15:34:34 -0600

    Added support for missing or empty response type for client approval

--- index.js
+++ index.js
@@ -67,7 +67,8 @@ const init = async () => {
 
         server.app.oauthOptions = {
             authGrantType: {
-                codeTTL: 1 // days
+                codeTTL: 1, // days
+                defaultResponseType: 'code'
             },
             jwt: {
                 exp: 315576000, // 10 years in seconds
